d05a9d98ca04b2d8277716a2fd1994553a72aaaa
James Sargant <shorinbonsai@gmail.com>
2024-10-13 17:38:52 -0400

    can run external program

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
+use std::env;
 #[allow(unused_imports)]
 use std::io::{self, Write};
-// use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::process::Command;
 
 fn typey(slic: &str) {
-    let path_env = std::env::var("PATH").unwrap();
     match slic {
         "echo" => println!("{} is a shell builtin", slic),
         "type" => println!("{} is a shell builtin", slic),
         "exit" => println!("{} is a shell builtin", slic),
-        _ => println!("{}", path_env),
-        // _ => match path.exists() {
-        //     true => println!("{} is {}", slic, path.display()),
-        //     _ => println!("{}: not found", slic),
-        // }, // _ => println!("{}: not found", slic),
+        _ => match find_in_path(slic) {
+            Some(path) => println!("{}", path.display()),
+            None => println!("{slic}: not found"),
+        },
     }
+}
+
+fn find_in_path(command: &str) -> Option<PathBuf> {
+    if let Ok(paths) = env::var("PATH") {
+        for path in env::split_paths(&paths) {
+            let exe_path = path.join(command);
+            if exe_path.is_file() {
+                return Some(exe_path);
+            }
+        }
+    }
+    None
 }
 
 fn main() {
@@ -28,12 +40,22 @@ fn main() {
         let input = input.trim();
         let split: Vec<&str> = input.split(" ").collect();
         // println!("{}", first.unwrap());
+        let args: Vec<String> = split[1..].iter().map(|s| s.to_string()).collect();
 
         match split[..] {
             ["exit", "0"] => break,
             ["echo", ..] => println!("{}", split[1..].join(" ")),
             ["type", ..] => typey(split[1]),
-            _ => println!("{}: command not found", input.trim()),
+            [..] => {
+                if let Some(path) = find_in_path(split[0]) {
+                    Command::new(path)
+                        .args(args)
+                        .status()
+                        .expect("failed to execute");
+                } else {
+                    println!("{}: command not found", input.trim())
+                }
+            }
         }
     }
 }
